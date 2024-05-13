b8bbc665eb0101a1f373406f003e377c9d51c741
nikhilnimapinfo <166018854+nikhilnimapinfo@users.noreply.github.com>
2024-05-13 19:09:33 +0530

    Add files via upload

---
+++ 202405131059355_CreatePostData.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatePostData : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PostDatas",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        name = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.PostDatas");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_FrameWork_DataBase_FirstApproach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new shamEntities1();
+            Table s = new Table() { 
+            
+            addr="Pareal",
+                id = 11, 
+            };
+            j.Tables.Add(s);
+            j.SaveChanges();
+        }
+    }
+}
